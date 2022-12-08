5ab3a19f6d532953d1b91ca801f7ab940bbae5db
Eiger Li <eigerli@outlook.com>
2022-12-08 16:55:09 -0500

    restructure animation procedure, ready to add v-t plot

--- parameter/simulation.py
+++ parameter/simulation.py
@@ -10,7 +10,7 @@ import numpy as np
 N = 50        # number of vehicles      
 D = 1000           # length of the highway (m)        
 detail_range = (D//2 - 200, D//2 + 200) # range of the detailed plot
-T = 2400        # simulation time (s)
+T = 10        # simulation time (s)
 dt = 0.05       # time step (s)
 total_step = int(T/dt) # total number of steps
 
@@ -18,17 +18,37 @@ total_step = int(T/dt) # total number of steps
 use_smoothing = True
 
 # animation parameters
-display_animation = False
-save_animation = False 
+animation_types = ['vehicles', 'vt']
+animation_type = animation_types[1]
+display_animation = True        # show animation on screen
+save_animation = False          # save animation to file
+draw_animation = display_animation or save_animation    # draw animation
+
+if display_animation and not save_animation:
+    if animation_type == 'vehicles':
+        print("Displaying animation of vehicles...")
+    elif animation_type == 'vt':
+        print("Dislaying animation of v-t relation...")
+elif save_animation and not display_animation:
+    if animation_type == 'vehicles':
+        print("Saving animation of vehicles...")
+    elif animation_type == 'vt':
+        print("Saving animation of v-t relation...")
+elif not (display_animation or save_animation):
+    print("Not displaying or saving animation...")
+else:
+    raise ValueError("Invalid animation parameters!")    
+
+
 speedup = 100
 info_step = 5000         # print information every info_step frames
 
-# tracking parameters
+# x-t tracking parameters
 xt_track_iteration_step = 10
-# tracking x-t relation
 xt_track_vehicle_range = np.arange(0, max(N, 50), 10)
 
-# tracking v-t relation
+# v-t tracking parameters
+vt_track_iteration_step = 100
 vt_track_max_vehicle = 100
 if N < vt_track_max_vehicle:
     vt_track_vehicle_range = np.arange(0, N, 1)
--- simulation.py
+++ simulation.py
@@ -25,7 +25,7 @@ def init():
     return all_vehicles, detailed_vehicles
 
 #TODO: speed up animation by including multiple simulation iterations in one animation update
-def update(i, update_axis=True):
+def update(i):
     global loc, d, v, a # , onroad_mask
     # fig.canvas.resize_event()
     if (i+1) % info_step == 0 or i == total_step:
@@ -49,10 +49,13 @@ def update(i, update_axis=True):
 
     dov.dov_update(loc, d, v, a, use_smoothing=use_smoothing)
 
-    if update_axis:
-        # update ax[0] and ax[1]
-        all_vehicles.set_xdata(loc)
-        detailed_vehicles.set_xdata(loc)
+    if draw_animation:
+        if animation_type == 'vehicles':            
+            # update ax[0] and ax[1]
+            all_vehicles.set_xdata(loc)
+            detailed_vehicles.set_xdata(loc)
+        elif animation_type == 'vt':
+            pass 
 
     # traffic snake detection:
 
@@ -70,11 +73,19 @@ def update(i, update_axis=True):
         print(f"neg_v={v[neg_v_idx]}")
         raise ValueError("Negative velocity occurs.")
 
-    if update_axis:
-        # Due to blit=True, set_title will only work when the animation is saved as video
-        ax[0].set_title(f"0-{D}m (Full view) at time {i*dt:.2f}/{T:.2f}s")
-        ax[1].set_title(f"{detail_range[0]}-{detail_range[1]}m at time {i*dt:.2f}/{T:.2f}s")
-    return all_vehicles, detailed_vehicles
+    if draw_animation:
+        if animation_type == 'vehicles':
+            # Due to blit=True, set_title will only work when the animation is saved as video
+            ax[0].set_title(f"0-{D}m (Full view) at time {i*dt:.2f}/{T:.2f}s")
+            ax[1].set_title(f"{detail_range[0]}-{detail_range[1]}m at time {i*dt:.2f}/{T:.2f}s")
+        elif animation_type == 'vt':
+            pass 
+
+    if draw_animation:
+        if animation_type == 'vehicles':
+            return all_vehicles, detailed_vehicles
+        elif animation_type == 'vt':
+            pass 
 
 
 # loc, d, v, a = dummy_initialize()
@@ -85,87 +96,95 @@ loc, d, v, a = partial_highway_initialize(10)
 
 # animate vehicle movement
 
+if draw_animation:
+    if animation_type == 'vehicles':
 
-fig, ax = plt.subplots(2, 1)
+        fig, ax = plt.subplots(2, 1)
 
-ax[0].set_xlim(-D*0.1, D*1.1)
-ax[0].set_ylim(-1, LANE_WIDTH+1)
-ax[0].set_xlabel('Distance (m)')
-ax[0].set_ylabel('Lane')
-ax[0].grid(False)
-ax[0].set_aspect(60)
-ax[0].plot([0, D], [LANE_WIDTH, LANE_WIDTH], 'k')
-ax[0].set_title(f'All vehicles at time 0.00/{T:.2f}s')
-# plot a rectangle to represent the lane
-ax[0].add_patch(
-    Rectangle((0, 0), D, LANE_WIDTH, fill=False, edgecolor='k', linewidth=1)
-    )
-# plot a rectangle at detailed area
-ax[0].add_patch(
-    Rectangle((detail_range[0], 0), detail_range[1]-detail_range[0], LANE_WIDTH, fill=False, edgecolor='r', linewidth=2)
-    )
+        ax[0].set_xlim(-D*0.1, D*1.1)
+        ax[0].set_ylim(-1, LANE_WIDTH+1)
+        ax[0].set_xlabel('Distance (m)')
+        ax[0].set_ylabel('Lane')
+        ax[0].grid(False)
+        ax[0].set_aspect(60)
+        ax[0].plot([0, D], [LANE_WIDTH, LANE_WIDTH], 'k')
+        ax[0].set_title(f'All vehicles at time 0.00/{T:.2f}s')
+        # plot a rectangle to represent the lane
+        ax[0].add_patch(
+            Rectangle((0, 0), D, LANE_WIDTH, fill=False, edgecolor='k', linewidth=1)
+            )
+        # plot a rectangle at detailed area
+        ax[0].add_patch(
+            Rectangle((detail_range[0], 0), detail_range[1]-detail_range[0], LANE_WIDTH, fill=False, edgecolor='r', linewidth=2)
+            )
 
-# display 900m - 1100m of the highway in detail
-ax[1].set_xlim(*detail_range)
-ax[1].set_ylim(-1, LANE_WIDTH+1)
-ax[1].set_xlabel('Distance (m)')
-ax[1].set_ylabel('Lane')
-ax[1].set_title('Three-lane highway')
-ax[1].grid(False)
-ax[1].set_aspect(20)
-ax[1].add_patch(
-    Rectangle((detail_range[0], 0), detail_range[1]-detail_range[0], LANE_WIDTH, fill=False, edgecolor='r', linewidth=2)
-    )
-# turn off scale on x and y axes
-# ax.get_xaxis().set_visible(False)
-# ax.get_yaxis().set_visible(False)
-ax[1].set_title(f"{detail_range[0]}-{detail_range[1]}m at time 0.00/{T:.2f}s")
+        # display 900m - 1100m of the highway in detail
+        ax[1].set_xlim(*detail_range)
+        ax[1].set_ylim(-1, LANE_WIDTH+1)
+        ax[1].set_xlabel('Distance (m)')
+        ax[1].set_ylabel('Lane')
+        ax[1].set_title('Three-lane highway')
+        ax[1].grid(False)
+        ax[1].set_aspect(20)
+        ax[1].add_patch(
+            Rectangle((detail_range[0], 0), detail_range[1]-detail_range[0], LANE_WIDTH, fill=False, edgecolor='r', linewidth=2)
+            )
+        # turn off scale on x and y axes
+        # ax.get_xaxis().set_visible(False)
+        # ax.get_yaxis().set_visible(False)
+        ax[1].set_title(f"{detail_range[0]}-{detail_range[1]}m at time 0.00/{T:.2f}s")
 
-# plot location of vehicles
-# scatter with alternating colors of red and blue
-all_vehicles, = ax[0].plot(
-    loc, 
-    np.ones(N) * LANE_WIDTH / 2, 
-    marker="o", markersize=1, ls="None",)
-# for i in range(N):
-#     all_vehicles.set_color(color[i])
-detailed_vehicles, = ax[1].plot(
-    loc, 
-    np.ones(N) * LANE_WIDTH/2,
-    marker="o", markersize= np.min([5, 5 / ((detail_range[1]-detail_range[0])/200)]), ls="None")
+        # plot location of vehicles
+        # scatter with alternating colors of red and blue
+        all_vehicles, = ax[0].plot(
+            loc, 
+            np.ones(N) * LANE_WIDTH / 2, 
+            marker="o", markersize=1, ls="None",)
+        # for i in range(N):
+        #     all_vehicles.set_color(color[i])
+        detailed_vehicles, = ax[1].plot(
+            loc, 
+            np.ones(N) * LANE_WIDTH/2,
+            marker="o", markersize= np.min([5, 5 / ((detail_range[1]-detail_range[0])/200)]), ls="None")
 
-# Print parameters
-print("Simulation time: {}s\ndt: {:.2f}s\n#Update: {}\nAnimation speedup: {}".format(
-    T, dt, total_step, speedup))
-print(f"Number of vehicles: {N}")
-print(f"Road length: {D}m")
-print(f"Save animation: {save_animation}")
+        # Print parameters
+        print("Simulation time: {}s\ndt: {:.2f}s\n#Update: {}\nAnimation speedup: {}".format(
+            T, dt, total_step, speedup))
+        print(f"Number of vehicles: {N}")
+        print(f"Road length: {D}m")
+        print(f"Save animation: {save_animation}")
 
-if display_animation or save_animation:
-    ani = animation.FuncAnimation(fig, update, init_func=init, frames=total_step, interval=int(dt*1000/speedup), blit=True, repeat=False)
-    if save_animation:
-        ani.save('./results/animation.mp4', writer='ffmpeg', fps=240)
-    else:
-        plt.show()
+        ani = animation.FuncAnimation(fig, update, init_func=init, frames=total_step, interval=int(dt*1000/speedup), blit=True, repeat=False)
+
+        if save_animation:
+            ani.save('./results/animation.mp4', writer='ffmpeg', fps=240)
+        else:
+            plt.show()
+
+elif animation_type == 'vt':
+    pass 
+
 else:
     xt_track_vehicle_loc = np.zeros((total_step//xt_track_iteration_step, N))
-    xt_track_vehicle_loc = np.zeros((total_step//xt_track_iteration_step, N))
-    # vt_track_vehicle_velocity = np.zeros((total_step//vt_track_iteration_step, N))
+    vt_track_vehicle_velocity = np.zeros((total_step//vt_track_iteration_step, N))
     metric_mean_velocity = np.zeros(total_step//xt_track_iteration_step)
     metric_std_velocity = np.zeros(total_step//xt_track_iteration_step)
 
     for i in range(total_step):
-        update(i, update_axis=False)
+        update(i)
         if i % xt_track_iteration_step == 0:
             xt_track_vehicle_loc[i//xt_track_iteration_step, :] = loc
             metric_mean_velocity[i//xt_track_iteration_step] = np.mean(v)
             metric_std_velocity[i//xt_track_iteration_step] = np.std(v)
-    
-    all_vehicles.set_xdata(loc)
-    detailed_vehicles.set_xdata(loc)
+
+        if i % vt_track_iteration_step == 0:
+            vt_track_vehicle_velocity[i//vt_track_iteration_step, :] = v
+
+    # all_vehicles.set_xdata(loc)
+    # detailed_vehicles.set_xdata(loc)
     fig2, ax2 = plt.subplots(1, 1)      # plot x-t relation
-    fig3, ax3 = plt.subplots(2, 1)      # plot mean and std of velocity
-    # fig4, ax4 = plt.subplots(1, 1)      # plot v-t relation and traffic snake
+    fig3, ax3 = plt.subplots(1, 1)      # plot mean and std of velocity
+    fig4, ax4 = plt.subplots(2, 1)      # plot v-t relation and traffic snake
 
     # plot x-t relation
     for n in xt_track_vehicle_range:
@@ -183,21 +202,31 @@ else:
     ax2.set_title('Location of vehicles')
     ax2.legend()
 
+    # plot v-t relation
+    for m in range(vt_track_vehicle_velocity.shape[0]):
+        ax3.plot(
+            vt_track_vehicle_range,
+            vt_track_vehicle_velocity[m, :], label=f"Time {m*vt_track_iteration_step*dt:.2f}s")
+    ax3.set_xlabel('Time (s)')
+    ax3.set_ylabel('Velocity (m/s)')
+    ax3.set_title('Velocity of vehicles')
+    ax3.legend()
+
     # plot mean(v)-t relation
-    ax3[0].plot(
+    ax4[0].plot(
         np.linspace(0, T, total_step//xt_track_iteration_step), metric_mean_velocity)
     # vmax as reference
-    ax3[0].plot(np.linspace(0, T, total_step//xt_track_iteration_step), np.ones(total_step//xt_track_iteration_step)*vmax, 'r--')
-    ax3[0].set_xlabel('Time (s)')
-    ax3[0].set_ylabel('Mean velocity (m/s)')
-    ax3[0].set_title('Mean velocity of vehicles')
+    ax4[0].plot(np.linspace(0, T, total_step//xt_track_iteration_step), np.ones(total_step//xt_track_iteration_step)*vmax, 'r--')
+    ax4[0].set_xlabel('Time (s)')
+    ax4[0].set_ylabel('Mean velocity (m/s)')
+    ax4[0].set_title('Mean velocity of vehicles')
 
     # plot std(v)-t relation
-    ax3[1].plot(
+    ax4[1].plot(
         np.linspace(0, T, total_step//xt_track_iteration_step), metric_std_velocity)
-    ax3[1].set_xlabel('Time (s)')
-    ax3[1].set_ylabel('Std velocity (m/s)')
-    ax3[1].set_title('Std velocity of vehicles')
+    ax4[1].set_xlabel('Time (s)')
+    ax4[1].set_ylabel('Std velocity (m/s)')
+    ax4[1].set_title('Std velocity of vehicles')
 
     
 
